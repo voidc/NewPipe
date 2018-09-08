72c68907b8ff5a22e2cddb17ba13f3494ffa968b
Dominik Stolz <d.stolz@tum.de>
2018-09-09 01:15:18 +0200

    Added query method to get information from the download manager

--- app/src/main/java/org/schabi/newpipe/download/NewPipeDownloadManager.java
+++ app/src/main/java/org/schabi/newpipe/download/NewPipeDownloadManager.java
@@ -2,6 +2,7 @@ package org.schabi.newpipe.download;
 
 import android.app.DownloadManager;
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
 import io.reactivex.Flowable;
 import io.reactivex.Maybe;
@@ -51,5 +52,81 @@ public class NewPipeDownloadManager {
 
     public Flowable<List<DownloadEntry>> getDownloads() {
         return downloadTable.getDownloads().subscribeOn(Schedulers.io());
+    }
+
+    public Maybe<DownloadInfo> query(long downloadId) {
+        final DownloadManager.Query query = new DownloadManager.Query().setFilterById(downloadId);
+        return Maybe.fromCallable(() -> {
+            Cursor result = downloadManager.query(query);
+            if(result.moveToFirst()) {
+                Uri localUri = Uri.parse(result.getString(
+                        result.getColumnIndexOrThrow(DownloadManager.COLUMN_LOCAL_URI)));
+
+                String mediaType = result.getString(
+                        result.getColumnIndexOrThrow(DownloadManager.COLUMN_MEDIA_TYPE));
+
+                DownloadStatus status = DownloadStatus.fromCode(result.getInt(
+                        result.getColumnIndexOrThrow(DownloadManager.COLUMN_STATUS)));
+
+                int downloadedBytes = result.getInt(
+                        result.getColumnIndexOrThrow(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
+
+                int totalBytes = result.getInt(
+                        result.getColumnIndexOrThrow(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
+
+                result.close();
+                return new DownloadInfo(localUri, mediaType, status, downloadedBytes, totalBytes);
+            } else {
+                return null;
+            }
+        }).subscribeOn(Schedulers.io());
+    }
+
+    public static class DownloadInfo {
+        final public Uri localUri;
+        final public String mediaType;
+        final public DownloadStatus status;
+        final public int downloadedBytes;
+        final public int totalBytes;
+
+        public DownloadInfo(Uri localUri, String mediaType, DownloadStatus status, int downloadedBytes, int totalBytes) {
+            this.localUri = localUri;
+            this.mediaType = mediaType;
+            this.status = status;
+            this.downloadedBytes = downloadedBytes;
+            this.totalBytes = totalBytes;
+        }
+
+        @Override
+        public String toString() {
+            return "DownloadInfo{" +
+                    "localUri=" + localUri +
+                    ", mediaType='" + mediaType + '\'' +
+                    ", status=" + status +
+                    ", downloadedBytes=" + downloadedBytes +
+                    ", totalBytes=" + totalBytes +
+                    '}';
+        }
+    }
+
+    public enum DownloadStatus {
+        FAILED(DownloadManager.STATUS_FAILED),
+        PAUSED(DownloadManager.STATUS_PAUSED),
+        PENDING(DownloadManager.STATUS_PENDING),
+        RUNNING(DownloadManager.STATUS_RUNNING),
+        SUCCESSFUL(DownloadManager.STATUS_SUCCESSFUL);
+
+        final private int code;
+        DownloadStatus(int code) {
+            this.code = code;
+        }
+
+        static DownloadStatus fromCode(int code) {
+            for(DownloadStatus status : DownloadStatus.values()) {
+                if(code == status.code)
+                    return status;
+            }
+            throw new IllegalArgumentException("Illegal status code: " + code);
+        }
     }
 }
